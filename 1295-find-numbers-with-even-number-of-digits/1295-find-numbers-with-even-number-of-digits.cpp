e10b5fd1539e324a079527f93e9976c3ae9dbf33
parshvi jain <163634684+parshvi-12345@users.noreply.github.com>
2026-07-09 15:28:32 +0530

    Time: 0 ms (100%), Space: 13.3 MB (66.16%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -13,20 +13,6 @@ public:
     }
 
     int findNumbers(vector<int>& nums) {
-        //     for (int i=0;i<nums.size();i++){
-        //         int count_digits(nums){
-        //             if (nums[i]==0){
-        //                 return 0;
-        //             }
-        //             else{
-        //                 return 1+digits(num[i]/10)
-        //             }
-        //         if (nums[i]%2==0){
-        //             count++;
-        //         }
-        //     }
-        // }     
-        // return count;
         int count=0;
         for(int i=0;i<nums.size();i++)
         {
